e72b5475c599000861611d60e0d8b678c369cb52
koujiang <missyouling@gmail.com>
2026-09-26 13:45:11 +0800

    fix(sandbox): 修复能耗管理列表业务字段映射脱节导致的数据列空白问题

--- frontend/src/views/dailyAffairs/SolarManagement.vue
+++ frontend/src/views/dailyAffairs/SolarManagement.vue
@@ -1,4 +1,4 @@
-<template>
+﻿<template>
   <div class="solar-management-container">
     <input ref="fileInputRef" type="file" multiple accept=".pdf,.jpg,.jpeg,.png" style="display: none"
       @change="onFileInputChange" />
@@ -97,7 +97,7 @@
         @row-click="({ row }: any) => handleRowClick(row)"
         @select-change="(val: any) => onTableSelectChange(val)"
       >
-        <template #defaultCell="{ row, col }: any">
+        <template #default="{ row, col }: any">
           <span v-if="row.kind === 'pending' && col.colKey === visibleColDefs[0]?.key" class="row-text" :title="row.fileName">{{ row.fileName }}</span>
           <span v-else-if="col.fieldType === 'number' || col.fieldType === 'amount'" class="row-mono" :title="cellText(row, col.colKey)">
             {{ cellText(row, col.colKey) }}
--- frontend/src/views/dailyAffairs/UtilitiesManagement.vue
+++ frontend/src/views/dailyAffairs/UtilitiesManagement.vue
@@ -150,7 +150,7 @@
           @row-click="({ row }: any) => handleRowClick(row)"
           @select-change="(val: any) => onTableSelectChange(val)"
         >
-          <template #defaultCell="{ row, col }: any">
+          <template #default="{ row, col }: any">
             <span v-if="row.kind === 'pending' && col.colKey === visibleColDefs[0]?.key" class="row-text" :title="row.fileName">{{ row.fileName }}</span>
             <span v-else-if="col.fieldType === 'number' || col.fieldType === 'amount'" class="row-mono" :title="cellText(row, col.colKey)">
               {{ cellText(row, col.colKey) }}
